c90765dfa4bc85c5d83a23b82a47e38ffd6a5ce4
George Gorchev <georgegorchev@yandex.ru>
2021-01-15 03:09:02 +0300

    add: url regex

---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+// name — имя карточки, строка от 2 до 30 символов, обязательное поле;
+// link — ссылка на картинку, строка, обязательно поле. Для проверки данных используйте регулярное выражение из предыдущей схемы;
+// owner — ссылка на модель автора карточки, тип ObjectId, обязательное поле;
+// likes — список лайкнувших пост пользователей, массив ObjectId, по умолчанию — пустой массив (поле default);
+// createdAt — дата создания, тип Date, значение по умолчанию Date.now.
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true
+    // добавить регулярку сюда
+  },
+  owner: {
+    type: ObjectId,
+    required: true
+    // добавить регулярку сюда
+  },
+  likes: {},
+  createdAt: {
+    type: Date,
+  }
+});
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
-// models/user.js
 const mongoose = require('mongoose');
+
+const regex = new RegExp(/^(https?:\/\/)?(w{3}.)?+([a-z\d]{0,}).([a-z\d]{2,})+([a-z0-9-._~:\/?#[\\\]@!\$&'()*+,;=]{1,})/gi)
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,7 +16,15 @@ const userSchema = new mongoose.Schema({
     maxlength: 30,
   },
   avatar: {
-    type: String
+    type: String,
+    validate: {
+      validator(url) {
+          return regex.test(url);
+      },
+      message: 'URL адрес для изображения указан некорректно.',
+  }
     // добавить регулярку сюда
   }
 });
+
+module.exports = mongoose.model('user', userSchema);
