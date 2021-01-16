2e02aa549138de86777edcc285b56d3e42f0016a
George Gorchev <georgegorchev@yandex.ru>
2021-01-17 00:13:47 +0300

    add: PATCH updateUser / PATCH updateUserAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -28,4 +28,22 @@ const createUser = (req, res) => {
     .catch((err) => handleError(err, res, 'пользователя'));
 };
 
-module.exports = { getUsers, getUsersById, createUser };
+// PATCH Обновляет данные пользователя:
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((updated) => res.send({ data: updated }))
+    .catch((err) => handleError(err, res, 'пользователя'));
+};
+
+// PATCH Обновляет аватар пользователя:
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => handleError(err, res, 'пользователя'));
+};
+
+module.exports = {
+  getUsers, getUsersById, createUser, updateUser, updateUserAvatar,
+};
