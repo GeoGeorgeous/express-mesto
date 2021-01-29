4368934b6e532a60f0724e85721afe69cb204bd1
George Gorchev <georgegorchev@yandex.ru>
2021-01-29 16:23:12 +0300

    feature: auth middelware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const userRouter = require('./routes/userRouter');
 const cardRouter = require('./routes/cardRouter');
@@ -28,8 +29,8 @@ app.use((req, res, next) => {
 // Роутинг:
 app.post('/signin', login);
 app.post('/signup', createUser);
-app.use('/', userRouter); // Роутинг пользователей
-app.use('/', cardRouter); // Роутинг карточек
+app.use('/', auth, userRouter); // Роутинг пользователей
+app.use('/', auth, cardRouter); // Роутинг карточек
 app.use('*', (req, res) => { // Роутинг 404
   res.status('404').json({ message: 'Запрашиваемый ресурс не найден' });
 });
