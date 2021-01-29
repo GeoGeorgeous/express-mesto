2e991275cb25656ae3ed3d9e9b44c25f2e56cfc9
George Gorchev <georgegorchev@yandex.ru>
2021-01-29 13:36:02 +0300

    fix: userSchema

--- models/user.js
+++ models/user.js
@@ -1,18 +1,31 @@
-const mongoose = require('mongoose');
+import { isEmail } from 'validator';
+
+const mongoose = require('mongoose'); // Валидтор для Email
 const urlRegExp = require('../utils/regExp'); // Регулярное выражение для проверки URL
 
 const userSchema = new mongoose.Schema({
-  name: { // Имя пользователя
+  email: { // Email пользователя
     type: String,
     required: true,
+    unique: true,
+    validate: [isEmail, 'Неправильно указан email'],
+  },
+  password: { // Пароль пользователя
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  name: { // Имя пользователя
+    type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: { // Информация о пользователе
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: { // Ссылка на аватар
     type: String,
@@ -22,6 +35,7 @@ const userSchema = new mongoose.Schema({
       },
       message: 'URL адрес для изображения указан некорректно.',
     },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
